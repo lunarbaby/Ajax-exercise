44a88383ff6734142bf07109d013545d2b914d6c
Lunarbaby <lunarbaby21@hotmail.com>
2019-03-04 15:26:35 -0500

    change website location and added headers

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: "origin, x-requested-with, content-type"');
+header('Access-Control-Allow-Methods "GET, POST, OPTIONS"');
+
+if(isset($_POST['userName']) && !empty($_POST['userName'])) {
+  $name = $_POST['userName'];
+  $computedString = "Post Success: Hi, " . $name . "!";
+} else if(isset($_GET['userName']) && !empty($_GET['userName'])) {
+  $name = $_GET['userName'];
+  $computedString = "GET Success: Hi, " . $name . "!";
+} else if ($_POST) {
+  $name = $_POST;
+  $computedString = "Seems like there was something wrong with your POST " . $name . "!";
+} else if ($_GET) {
+  $name = $_GET;
+  $computedString = "Seems like there was something wrong with your GET " . $name . "!";
+} else {
+  $name = "no name";
+  $computedString = "Seems like my error handling is broke " . $name . "!";
+}
+// $name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
+// $computedString = "Hi, " . $name . "!";
+$array = ['userName' => $name, 'computedString' => $computedString];
+echo json_encode($array);
+?>
